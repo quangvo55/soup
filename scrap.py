813c922d5783852e01236e781cde5a491614c8ad
James Vo <quangvo55@gmail.com>
2014-06-18 09:37:03 -0700

    2nd

--- scrap.py
+++ scrap.py
@@ -12,20 +12,21 @@ def getData(url):
 		f.write('\n')
 		strrow = '';
 		for i in range(len(children)):
-			if (i == 2 or i ==4):
+			if (i == 2 or i ==4): #skip duplicate values
 				continue
 			child = children[i]
 			value = str(child.text).replace('\n', ',').replace('\r', '')
 			value = " ".join(value.split())
 			strrow += value
-		strrow = re.sub(r',\s|\s,', ',', strrow).replace(',,', ',')
-		strrow = re.sub(r'^,', '', strrow)
-		strrow = re.sub(r'(L|W)\s', r'\1,', strrow)
-		strrow = re.sub(r'(O|U)\s', r'\1,', strrow)
+		#clean and transform data to desire format for csv file
+		strrow = re.sub(r',\s|\s,', ',', strrow).replace(',,', ',') #remove whitespaces
+		strrow = re.sub(r'^,', '', strrow) 
+		strrow = re.sub(r'(L|W)\s', r'\1,', strrow) #seprate game outcomes into two columns
+		strrow = re.sub(r'(O|U)\s', r'\1,', strrow) #seperate OU outcomes into two columns
 		f.write(strrow)
 	return
 
-f = open('test', 'w')
+f = open('test2', 'w')
 f.write('Date,Team,Outcome,Score,Gametype,Spread Outcome,Spread,OU OutCome,OU')
 years = ['2013-2014', '2012-2013', '2011-2012', '2010-2011', '2009-2010', '2008-2009', '2007-2008', '2006-2007', '2005-2006', '2004-2005']
 for i in range(len(years)):
